e0b9eea9c16da8765ec538bb15d5a1a18a6218d5
Nikhil Menon <nikhilmenon@Nikhils-MacBook-Pro.local>
2020-06-26 12:33:46 -0400

    minor change in run file

--- Run_Lottery_Sim.py
+++ Run_Lottery_Sim.py
@@ -1,11 +1,11 @@
-import NHL_Lottery_Sim as lottery
+import NHL_Lottery_Sim as nhl_lottery
 
 lottery_prob = {1: [.185, .165, .144], 2: [.135, .13, .123], 3: [.115, .113, .111],
                 4: [.095, .096, .097], 5: [.085, .087, .089], 6: [.075, .078, .08], 7: [.065, .068, .071],
                 8: [.06, .063, .067], 9: [.05, .053, .057], 10: [.035, .038, .041], 11: [.03, .033, .036],
                 12: [.025, .027, .03], 13: [.02, .022, .024], 14: [.015, .017, .018], 15: [.01, .011, .012]}
 
-#lottery_prob = lottery.lottery_prob
+#lottery_prob = nhl_lottery.lottery_prob
 
-lottery_order = lottery.reverse_standings(lottery.get_standings())
-print(lottery.draft_order(lottery_prob, lottery_order))
+lottery_order = nhl_lottery.reverse_standings(nhl_lottery.get_standings())
+print(nhl_lottery.draft_order(lottery_prob, lottery_order))
